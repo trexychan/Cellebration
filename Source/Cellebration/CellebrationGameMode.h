826feecef5535d2889357d53b44d56d9a984b39d
ActuallyTrue <dotaigbe@icloud.com>
2020-06-26 17:25:03 -0500

    Initial commit

---
+++ Source/Cellebration/CellebrationGameMode.h
@@ -0,0 +1,19 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CellebrationGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ACellebrationGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ACellebrationGameMode();
+};
+
+
+
